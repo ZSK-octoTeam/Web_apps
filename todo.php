6a09c9a8b40ad791c6655d452f7a68ecfb3f2660
Filip <Filip2007@wp.pl>
2025-01-14 23:20:35 +0100

    Add a .sql file and method to add from form to database

--- todo.php
+++ todo.php
@@ -12,13 +12,32 @@
     <section>
         <form action="" method="Post">
             <label for="namee">
-
+                <input type="text" name="namee" placeholder="Task name">
             </label>
             <label for="datee">
-
+                <input type="date" name="datee">
             </label>
             <input type="submit" value="Add">
         </form>
+        <?php
+            if(isset($_POST["namee"]) && !empty($_POST["datee"])){
+                $conn = mysqli_connect("localhost", "root", "", "to_do");
+                if(mysqli_connect_error()){
+                    die("Failed to connect to database" . mysqli_connect_error());
+                }
+                else{
+                    $name = $_POST["namee"];
+                    $date = $_POST["datee"];
+                    $sql = "INSERT INTO `tasks`(`Name`, `Time`) VALUES('$name', '$date')";
+                    mysqli_query($conn, $sql);
+                }
+                mysqli_close($conn);
+            }else{
+                echo "<script>
+                alert('Błędne dane w formularzu');
+                </script>";
+            }
+        ?>
     </section>
     <main>
 
