2bc15df013530e62d028123b61f9191b11c8684b
Guc10 <gustaw.grzeskowiak@gmail.com>
2025-01-14 23:28:21 +0100

    HTML frame

--- todo.php
+++ todo.php
@@ -3,21 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>To Do List</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.scss">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="script.js"></script>
 </head>
 <body>
     <header>
-        <h1>To do list</h1>
+        <h1>to do list</h1>
     </header>
     <section>
         <form action="" method="Post">
-            <label for="namee">
-
-            </label>
-            <label for="datee">
-
-            </label>
-            <input type="submit" value="Add">
+            <input class="input" name="namee" placeholder="task name..." type="text">
+            <input class="input" name="datee" type="date">
+            <input class="input" type="submit" value="add">
         </form>
     </section>
     <main>
