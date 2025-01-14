1b4588b1e4ecbc1b554c05bb8367ec77ab43eba8
Guc10 <gustaw.grzeskowiak@gmail.com>
2025-01-14 22:40:57 +0100

    HTML frame

--- todo.php
+++ todo.php
@@ -6,6 +6,22 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>To do list</h1>
+    </header>
+    <section>
+        <form action="" method="Post">
+            <label for="namee">
+
+            </label>
+            <label for="datee">
+
+            </label>
+            <input type="submit" value="Add">
+        </form>
+    </section>
+    <main>
+
+    </main>
 </body>
 </html>
